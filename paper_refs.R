40e0b7128c57f4d25cc9c6fbfcd689cec3a66c9f
Mark Scheuerell <scheuerl@uw.edu>
2021-12-17 06:08:28 -0800

    moved code to plain script

---
+++ paper_refs.R
@@ -0,0 +1,27 @@
+## load the package
+library("knitcitations")
+
+## clean out any existing .bib files
+cleanbib()
+
+## set pandoc for formatting
+options("citation_format" = "pandoc")
+
+
+## Citations
+
+citep("10.1146/annurev.ecolsys.33.010802.150504")
+
+citep("10.1577/1548-8659(1986)115<662:SASAIT>2.0.CO;2")
+
+citep("10.1111/j.1526-100X.2007.00245.x")
+
+citep("10.1139/cjfas-2016-0344")
+
+citep("10.1002/ece3.1509")
+
+citep("10.1890/ES14-00466.1")
+
+
+## create ref cited section
+write.bibtex(file = "mark_references.bib")
