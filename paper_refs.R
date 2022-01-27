5fc52304b88f93e089d86707d6e5bf841bb21f2d
Mark Scheuerell <scheuerl@uw.edu>
2022-01-27 06:33:06 -0800

    added more refs

--- paper_refs.R
+++ paper_refs.R
@@ -24,6 +24,19 @@ citep("10.1890/ES14-00466.1")
 
 citep("10.1016/j.jenvman.2021.113536")
 
-
 ## create ref cited section
 write.bibtex(file = "mark_references.bib")
+
+
+#### round 2 ####
+
+citep("10.1002/jwmg.21880")
+
+citep("10.1002/eap.2193")
+
+citep("10.1002/ecs2.3618")
+
+citep("10.1111/j.1523-1739.2005.00300.x")
+
+## create ref cited section
+write.bibtex(file = "mark_references_v2.bib")
